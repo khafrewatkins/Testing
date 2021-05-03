ac62620ca84b87fdcb4f0c2a6eace355e3626a1f
Ryan Booth <ryanboothforever@gmail.com>
2021-05-03 07:42:29 -0500

    Fixed feed.ejs and added likes front end functionallity to public/ main.js

---
+++ public/js/main.js
@@ -0,0 +1,26 @@
+const liked = document.querySelectorAll(".fa-heart");
+
+Array.from(liked).forEach((element) => {
+  element.addEventListener("click", addLike);
+});
+
+async function addLike() {
+  const likedEntry = this.parentNode.parentNode.childNodes[1].childNodes[1]
+    .childNodes[1].innerText;
+  const tLikes = Number(this.parentNode.childNodes[3].innerText);
+  try {
+    const response = await fetch("addOneLike", {
+      method: "put",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify({
+        stageNameS: likedEntry,
+        likes: tLikes,
+      }),
+    });
+    const data = await response.json();
+    console.log(data);
+    location.reload();
+  } catch (err) {
+    console.log(err);
+  }
+}
