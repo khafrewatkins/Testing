82339593f24ef97211adc7f622e03b3e78237cf5
AngelGDev <44039186+AngelGDev@users.noreply.github.com>
2021-05-03 19:46:15 -0700

    updated model to include author name

--- models/Entry.js
+++ models/Entry.js
@@ -27,6 +27,10 @@ const EntrySchema = new mongoose.Schema({
     type: mongoose.Schema.Types.ObjectId,
     ref: "User",
   },
+  author: {
+    type: String,
+    required: true,
+  },
   createdAt: {
     type: Date,
     default: Date.now,
